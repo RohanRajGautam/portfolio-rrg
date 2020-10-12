1557de911853a1a1ddeee09204891281c085fa7e
rohan <rohanrajgautam@gmail.com>
2020-10-12 09:02:49 +0545

    changed env-config

--- env-config.js
+++ env-config.js
@@ -1,7 +1,7 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  'process.env.BASE_URL': prod ? 'https://filipjerga.herokuapp.com' : 'http://localhost:3000',
-  'process.env.NAMESPACE': 'https://filipjerga.herokuapp.com',
-  'process.env.CLIENT_ID': 'NfvS9nw81ItncHJKPHCaAvwD9ChNWYn3'
+  'process.env.BASE_URL': prod ? 'https://iamrrg.herokuapp.com' : 'http://localhost:3000',
+  'process.env.NAMESPACE': 'https://iamrrg.herokuapp.com',
+  'process.env.CLIENT_ID': 'xqrJSn3zeUqhI6rJofLmitmHOimNI3vU'
 }
